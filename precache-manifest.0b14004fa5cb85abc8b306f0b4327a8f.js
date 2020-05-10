9a272fa44858a343d7871facffe9e8f0a5b21f1b
Bartek <bartekborowiec91@gmail.com>
2020-05-11 00:19:07 +0200

    Updates

---
+++ precache-manifest.0b14004fa5cb85abc8b306f0b4327a8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0493d2751b0c0eddd66519c28ad1450",
+    "url": "/player-with-annotations/index.html"
+  },
+  {
+    "revision": "5fc960c7f4ba8bca22a7",
+    "url": "/player-with-annotations/static/css/2.717faa4d.chunk.css"
+  },
+  {
+    "revision": "70db159314287fb60509",
+    "url": "/player-with-annotations/static/css/main.881d0929.chunk.css"
+  },
+  {
+    "revision": "5fc960c7f4ba8bca22a7",
+    "url": "/player-with-annotations/static/js/2.2bc5a3d0.chunk.js"
+  },
+  {
+    "revision": "7b08ac1b51fe7399a8d90ae875daaa26",
+    "url": "/player-with-annotations/static/js/2.2bc5a3d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70db159314287fb60509",
+    "url": "/player-with-annotations/static/js/main.5b172daa.chunk.js"
+  },
+  {
+    "revision": "66a7b401e2a6583a0d62",
+    "url": "/player-with-annotations/static/js/runtime-main.e171cc97.js"
+  }
+]);
